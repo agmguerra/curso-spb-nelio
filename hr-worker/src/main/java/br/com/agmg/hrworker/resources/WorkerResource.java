68e3fbc04bf7b9e1a2357e56395f0b1c83c10677
agmgu <agmguerra@gmail.com>
2021-05-12 23:23:37 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/br/com/agmg/hrworker/resources/WorkerResource.java
@@ -0,0 +1,43 @@
+package br.com.agmg.hrworker.resources;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.agmg.hrworker.entities.Worker;
+import br.com.agmg.hrworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+	private WorkerRepository repository;
+
+	public WorkerResource(WorkerRepository repository) {
+		super();
+		this.repository = repository;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll() {
+		
+		List<Worker> list = repository.findAll();
+		
+		return ResponseEntity.ok(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		Worker worker = repository.findById(id).get();
+		
+		return ResponseEntity.ok(worker);
+		
+	}
+	
+}
