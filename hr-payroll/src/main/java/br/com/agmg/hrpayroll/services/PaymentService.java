bf731fa1652d438d7c7f9e94fd380442c9c5cf0e
agmgu <agmguerra@gmail.com>
2021-05-13 23:19:09 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/agmg/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/agmg/hrpayroll/services/PaymentService.java
@@ -1,16 +1,35 @@
 package br.com.agmg.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.agmg.hrpayroll.entities.Payment;
+import br.com.agmg.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	private String workerHost;
+	
+	private RestTemplate restTemplate;
+	
+	public PaymentService(RestTemplate restTemplate, @Value("${hr-worker.host}") String workerHost) {
+		this.workerHost = workerHost;
+		this.restTemplate = restTemplate;
+	}
+	
 	public Payment getPayment(Long workerId, int days) {
 		
-		//Mock para implementacao inicial
-		return new Payment("Bob", 200.0, days);
+		Map <String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+			
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
 
