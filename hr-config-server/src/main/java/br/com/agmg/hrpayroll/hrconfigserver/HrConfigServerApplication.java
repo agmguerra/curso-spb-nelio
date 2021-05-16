d48414bdd72ce57eb072bb782e1dacdc313ae4b7
Alexandre Guerra <agmguerra@gmail.com>
2021-05-16 14:21:40 -0300

    Config Server created

---
+++ hr-config-server/src/main/java/br/com/agmg/hrpayroll/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.agmg.hrpayroll.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
---
+++ hr-config-server/src/test/java/br/com/agmg/hrpayroll/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.hrpayroll.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
