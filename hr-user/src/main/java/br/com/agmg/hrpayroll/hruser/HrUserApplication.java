d06ee91bb720086568a149c39a8d4651b268e068
Alexandre Guerra <agmguerra@gmail.com>
2021-05-17 00:28:39 -0300

    Database seeding

--- hr-user/src/main/java/br/com/agmg/hrpayroll/hruser/HrUserApplication.java
+++ hr-user/src/main/java/br/com/agmg/hrpayroll/hruser/HrUserApplication.java
@@ -1,15 +1,33 @@
 package br.com.agmg.hrpayroll.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
+	
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {	
+		//System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+	}
+
+	@Autowired
+	public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	
 }
